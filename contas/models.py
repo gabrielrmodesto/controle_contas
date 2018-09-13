30d1faffbe40f90fea92d25c858088fad951e3e9
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-09-13 15:08:15 +0000

    Classe de transação implentada

--- contas/models.py
+++ contas/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+
+
+class Transacao(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=300)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacao = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Transações'
