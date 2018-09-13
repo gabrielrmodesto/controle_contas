9ada173df1bbd821c6b27af70fdbe851937bcbea
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2018-09-13 18:28:40 +0000

    Implementada a forma da exibição da descrição e categoria

--- contas/models.py
+++ contas/models.py
@@ -5,16 +5,19 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name = 'Categoria'
+    def __str__(self):
+        return self.nome
 
 
 class Transacao(models.Model):
-    data = models.DateTimeField(auto_now_add=True)
+    data = models.DateTimeField()
     descricao = models.CharField(max_length=300)
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    observacao = models.TextField()
+    observacao = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return self.descricao
