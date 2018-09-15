419b44d1dfd9dd6fa0c7669ee6ee9c5fc268c496
GabrielRM <gabrielrmodesto5@gmail.com>
2018-09-15 00:01:44 -0300

    Parte de read do CRUD

--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Transacao
 import datetime
 
 def home(request):
     data = {}
-    data['transacoes'] = ['t1','t3','t2']
 
-    data['now'] = datetime.datetime.now()
     return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
