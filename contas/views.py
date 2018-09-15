5908e4391191f89e591018a9dc1e125e1a8866bc
GabrielRM <gabrielrmodesto5@gmail.com>
2018-09-14 23:16:33 -0300

    Utilizaçaõ do jinja no template

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def home(request):
-    return render(request, 'contas/home.html')
+    data = {}
+    data['transacoes'] = ['t1','t3','t2']
+
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/home.html', data)
